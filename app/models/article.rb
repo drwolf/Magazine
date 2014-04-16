8b0093fd1c057e9db38f124441f6159fb35c0829
Sterling Walker <lazarus_star@hotmail.com>
2014-04-15 22:09:56 -0400

    Regex adapted to capture root url apart from subdomain

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,12 @@ class Article < ActiveRecord::Base
 
   def self.find_articles(url)
     file = open(url)
-    regex = /<p>-- (.*)\(<a href="(.*)">(.*)<\/a>\).*<\/p>/
+    regex = /<p>-- (.*)\(<a href="(http.?:\/\/.*\.[a-z]{2,3}\/)(.*)">(.*)<\/a>\).*<\/p>/
+    new_articles = file.scan(regex)
+    new_articles.each do |article|
+
+      Article.create(title: article[0], subdomain: )
+    end
   end 
 
 end
