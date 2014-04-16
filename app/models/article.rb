afadf0eb2e999bb6502c8be04227f9efbf033a83
Sterling Walker <lazarus_star@hotmail.com>
2014-04-16 13:35:43 -0400

    Single line conditional syntax.

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,10 +7,8 @@ class Article < ActiveRecord::Base
     regex = /<p>-- (.*)\(<a href="(http.?:\/\/.*\.[a-z]{2,3}\/)(.*)">(.*)<\/a>\).*<\/p>/
     new_articles = file.scan(regex)
     new_articles.each do |article|
-      site = Site.find_by_url(article[1])
-      if site.length == 0
-        site = Site.create(title:article[3], url: article[1])
-      end
+      site = Site.find_by_url(article[1]).first
+      site = Site.create(title:article[3], url: article[1]) unless site
       article = Article.find_by_subdomain(article[2])
       Article.create(title: article[0], subdomain: article[2], site_id: site.id)
     end
