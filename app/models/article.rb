b9fd7ed28d97cedfd12ca32cd294a929d8f1c280
Sterling Walker <lazarus_star@hotmail.com>
2014-04-15 22:16:19 -0400

    Creates Site model as needed

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,12 @@ class Article < ActiveRecord::Base
     regex = /<p>-- (.*)\(<a href="(http.?:\/\/.*\.[a-z]{2,3}\/)(.*)">(.*)<\/a>\).*<\/p>/
     new_articles = file.scan(regex)
     new_articles.each do |article|
-
-      Article.create(title: article[0], subdomain: )
+      site = Site.find_by_url(article[1])
+      if site.length == 0
+        site = Site.create(title:article[3], url: article[1])
+      end
+      article = Article.find_by_subdomain(article[2])
+      Article.create(title: article[0], subdomain: article[2], site_id: site.id)
     end
   end 
 
