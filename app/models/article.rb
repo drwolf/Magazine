d82461f93ed53b1087ad25ce9eb4d090c5701c08
Sterling Walker <lazarus_star@hotmail.com>
2014-04-15 21:57:50 -0400

    Article finding regex

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
+require 'open-uri'
+
 class Article < ActiveRecord::Base
+
+  def self.find_articles(url)
+    file = open(url)
+    regex = /<p>-- (.*)\(<a href="(.*)">(.*)<\/a>\).*<\/p>/
+  end 
+
 end
